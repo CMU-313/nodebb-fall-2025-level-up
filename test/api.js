665e63e5d9d96caa7b41eecb80874e14ef5b0e2b
Alejandro Estrada <alejande@andrew.cmu.edu>
2025-11-07 21:11:43 +0000

    feat: merged UI changes for llm translation microservice

--- test/api.js
+++ test/api.js
@@ -689,7 +689,7 @@ describe('API', async () => {
 			}
 			
 			if (prop !== 'isAdminOrMod') {
-				assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context})`);
+				assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context}), SCHEMA ${JSON.stringify(schema)}`);
 			}
 		});
 	}
