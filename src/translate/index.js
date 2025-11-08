f49e11bfa9d575d9947f82757ebcc5de97a1a4eb
Alejandro Estrada <alejande@andrew.cmu.edu>
2025-11-08 01:55:33 +0000

    feat: added functionality for microservice

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,11 +1,22 @@
-
 /* eslint-disable strict */
-//var request = require('request');
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
+translatorApi.translate = async function (postData) {
+	const TRANSLATOR_API = process.env.TRANSLATOR_API || 'http://127.0.0.1:5000';
+
+	try {
+		const response = await fetch(`${TRANSLATOR_API}/?content=${encodeURIComponent(postData.content)}`);
+		const data = await response.json();
+
+		console.log('[translator] sending to:', `${TRANSLATOR_API}/?content=${postData.content}`);
+		// Flask returns {"is_english": bool, "translated_content": string}
+		return [data.is_english, data.translated_content];
+	} catch (err) {
+		console.error('Translation API error:', err);
+		// fallback: assume English, no translation
+		return [true, ''];
+	}
 };
 
 // translatorApi.translate = async function (postData) {
